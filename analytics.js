4623d17ccfbd49cef87220a2b2d18f06dbeae58f
x26589334-cpu <x26589334@gmail.com>
2026-06-25 10:57:37 +0900

    Install Google Analytics 4 (G-RQ5Q4ZKWQ6) across all pages via analytics.js

---
+++ analytics.js
@@ -0,0 +1,13 @@
+// Google Analytics 4 (GA4)
+(function () {
+  var GA_ID = 'G-RQ5Q4ZKWQ6';
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = 'https://www.googletagmanager.com/gtag/js?id=' + GA_ID;
+  document.head.appendChild(s);
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ dataLayer.push(arguments); }
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', GA_ID);
+})();
